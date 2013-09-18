bf16cde2181dc42bc978ddeb519d594c9e97cbeb
Jorge Gatica <jorge@macbook-de-macbook.local>
2013-09-18 04:32:45 -0400

    Clase 3.1, aca se empieza a crear un plugin. Hasta el momento, solo con agregar un archivo del plugin y en su codigo solamente unos comentarios , ya puedo ir al backend y ver que reconoce el archivo myFirstPlugin.php como un plugin

---
+++ wp-content/plugins/myFirstPlugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: El maravilloso plugin de Jorge
+Plugin URI: http://www.nsclick.cl
+Description: Este plugin puede ser fantastico
+Author: Jorge Gatica
+Version: 1.0
+Author URI: http://www.nsclick.cl/
+ */
+?>
