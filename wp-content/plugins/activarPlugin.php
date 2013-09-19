452d6889198ef49031ff1d0f69458751b03c8108
Jorge Gatica <j.gatica@yahoo.com>
2013-09-19 00:03:22 -0400

    Clase 3.4 - Vemos como aplicar un hook tipo acción, en este caso se utiliza el evento, activacion/desactivacion del plugin

---
+++ wp-content/plugins/activarPlugin.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Activacion Plugin
+Plugin URI: http://www.nsclick.cl
+Description: Este plugin hara algo excepcional cuando lo actives
+Author: Jorge Gatica
+Version: 1.0
+Author URI: http://www.nsclick.cl/
+*/
+function my_plugin_activate() {
+	//Esta funcion hara X
+	error_log("Has activado mi plugin");
+}
+
+register_activation_hook(__FILE__, "my_plugin_activate");
+
+function my_plugin_desactivate() {
+	error_log("Has desactivado mi plugin");
+}
+register_deactivation_hook(__FILE__, "my_plugin_desactivate");
