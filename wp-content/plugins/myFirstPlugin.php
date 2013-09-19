ce63981e2c4b55b3bb7d5dc5e0e2dec49009ddce
Jorge Gatica <j.gatica@yahoo.com>
2013-09-18 23:44:34 -0400

    Clase 3.3 - Determinar la versión de wordpress que esta siendo utilizada. Puse el código antes de activar el pulginf (version_compare(, 3,0,

--- wp-content/plugins/myFirstPlugin.php
+++ wp-content/plugins/myFirstPlugin.php
@@ -7,4 +7,10 @@ Author: Jorge Gatica
 Version: 1.0
 Author URI: http://www.nsclick.cl/
  */
+global $wp_version;
+#version_compare
+# esta es una funcion predefinida que permite comparar las versiones
+if (!version_compare($wp_version, "3,0",">=")) {
+	die("Necesitas una version posterior para este plugin");
+}
 ?>
